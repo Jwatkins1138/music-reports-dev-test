d01036a6c294fd147126ea0315b06dcafb49ecf8
Jason Watkins <goushoryuken@gmail.com>
2022-09-08 13:24:35 -0700

    update lastfm url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //lastfm api url
 const last = axios.create({
-  baseURL: 'http://ws.audioscrobbler.com',
+  baseURL: 'https://ws.audioscrobbler.com',
 });
 
 export { last };
